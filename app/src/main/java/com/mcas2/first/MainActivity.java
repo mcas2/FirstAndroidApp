25806805bb064cf00566c92751869985e8eaedcc
mcas2 <mcas2@email.com>
2024-10-18 13:25:05 +0200

    Registro y login actualizados, funcionando con SharedPreferences

--- app/src/main/java/com/mcas2/first/MainActivity.java
+++ app/src/main/java/com/mcas2/first/MainActivity.java
@@ -1,6 +1,10 @@
 package com.mcas2.first;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView welcomeText = findViewById(R.id.welcomeText);
+        TextView addedText = findViewById(R.id.addedText);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("Usuario", Context.MODE_PRIVATE);
+        String name = sharedPreferences.getString("userName", "anónimo");
+        String password = sharedPreferences.getString("userPassword", "contraseña");
+
+
+        welcomeText.setText("Hola "+name);
+        addedText.setText("Tu contraseña es "+password);
     }
 }
--- app/src/main/java/com/mcas2/first/Register.java
+++ app/src/main/java/com/mcas2/first/Register.java
@@ -1,12 +1,19 @@
 package com.mcas2.first;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class Register extends AppCompatActivity {
 
@@ -19,6 +26,31 @@ public class Register extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        TextInputLayout registerUsernameTIL = findViewById(R.id.registerUsernameTIL);
+        TextInputLayout registerPasswordTIL = findViewById(R.id.registerPasswordTIL);
+        TextInputLayout registerPasswordConfirmTIL = findViewById(R.id.registerPasswordConfirmTIL);
+        Button registerButton = findViewById(R.id.registerButton);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userName = String.valueOf(registerUsernameTIL.getEditText().getText());
+                String userPassword = String.valueOf(registerPasswordTIL.getEditText().getText());
+                String userPasswordCheck = String.valueOf(registerPasswordConfirmTIL.getEditText().getText());
+
+                if (!userPassword.equals(userPasswordCheck)) {
+                    Toast toast = Toast.makeText(getApplicationContext(), "Tus contraseñas no coinciden", Toast.LENGTH_SHORT);
+                    toast.show();
+                } else {
+                    SharedPreferences preferences = getSharedPreferences("Usuario", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putString("userName", userName);
+                    editor.putString("userPassword", userPassword);
+                    editor.apply();
+                }
+            }
         });
     }
 }
